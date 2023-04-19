b0606127d2d8e464d906a5eacb18342a983457be
Nguyễn Minh Huy <huynguyenminh.1106@gmail.com>
2023-04-20 00:36:13 +0700

    commit

--- Week 4/index-tailwind.js
+++ Week 4/index-tailwind.js
@@ -1,13 +1,17 @@
 var tabButtons = document.querySelectorAll(".tabContainer .buttonContainer button");
 var tabPanels = document.querySelectorAll(".tabContainer  .tabPanel");
+var tabButtons2 = document.querySelectorAll(".tabContainer2 .buttonContainer2 button");
+var tabPanels2 = document.querySelectorAll(".tabContainer2  .tabPanel2");
 
 function showPanel(panelIndex, colorCode) {
   tabButtons.forEach(function(node) {
     node.style.backgroundColor = "";
     node.style.color = "";
+    node.style.borderBottom = "1px solid #e4e7ed"
   });
   tabButtons[panelIndex].style.backgroundColor = colorCode;
   tabButtons[panelIndex].style.color = "#409eff";
+  tabButtons[panelIndex].style.borderBottom = "none";
   tabPanels.forEach(function(node) {
     node.style.display = "none";
   });
@@ -15,4 +19,36 @@ function showPanel(panelIndex, colorCode) {
   tabPanels[panelIndex].style.backgroundColor = colorCode;
 }
 
+function showPanel2(panelIndex, colorCode) {
+  tabButtons2.forEach(function(node) {
+    node.style.backgroundColor = "";
+    node.style.color = "";
+    node.style.borderBottom = "1px solid #e4e7ed"
+  });
+  tabButtons2[panelIndex].style.backgroundColor = colorCode;
+  tabButtons2[panelIndex].style.color = "#409eff";
+  tabButtons2[panelIndex].style.borderBottom = "none";
+  tabPanels2.forEach(function(node) {
+    node.style.display = "none";
+  });
+  tabPanels2[panelIndex].style.display = "block";
+  tabPanels2[panelIndex].style.backgroundColor = colorCode;
+}
+
+function hidePanel2(panelIndex){
+  tabButtons2.forEach(function(node) {
+    node.style.backgroundColor = "";
+    node.style.color = "";
+    node.style.borderBottom = "1px solid #e4e7ed"
+  });
+  tabPanels2.forEach(function(node) {
+    node.style.display = "none";
+  });
+  tabButtons2[panelIndex].style.borderBottom = "none";
+  tabButtons2[panelIndex].style.color = "#409eff";
+  // tabPanels2[panelIndex].style.display = "none";
+}
+
 showPanel(0, 'white');
+showPanel2(0, 'white');
+
