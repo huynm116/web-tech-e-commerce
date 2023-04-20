8dfac0bce9ac6433e3bed8d12878662f511f76da
Nguyễn Minh Huy <huynguyenminh.1106@gmail.com>
2023-04-20 21:43:13 +0700

    portfolio final

--- Week 4/index.js
+++ Week 4/index.js
@@ -15,8 +15,23 @@ function showPanel(panelIndex, colorCode) {
   tabPanels.forEach(function(node) {
     node.style.display = "none";
   });
+  tabPanels[panelIndex].style.visibility = "visible";
   tabPanels[panelIndex].style.display = "block";
   tabPanels[panelIndex].style.backgroundColor = colorCode;
+}
+
+function hidePanel(panelIndex){
+  tabButtons.forEach(function(node) {
+    node.style.backgroundColor = "";
+    node.style.color = "";
+    node.style.borderBottom = "1px solid #e4e7ed"
+  });
+  tabPanels.forEach(function(node) {
+    node.style.visibility = "hidden";
+  });
+  tabButtons[panelIndex].style.borderBottom = "none";
+  tabButtons[panelIndex].style.color = "#409eff";
+  // tabPanels2[panelIndex].style.display = "none";
 }
 
 function showPanel2(panelIndex, colorCode) {
